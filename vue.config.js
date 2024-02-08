36095d0a5c28381cfc9421f13220911eeb175fe8
Zal Amin <azmizalll123@gmail.com>
2024-02-08 12:28:25 +0800

    Vue2 Cli Host

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  publicPath: "http://localhost:8082",
+  chainWebpack: config => {
+    config.optimization.delete('splitChunks');
+    config
+    .plugin('module-federation-plugin')
+    .use(require('webpack').container.ModuleFederationPlugin, [{
+      remotes: {
+        "remote": "remote@http://localhost:8081/remoteEntry.js"
+      },
+      shared: {
+        "vue": { 
+          eager: true,
+          singleton: false
+        }
+      },
+    }])
+  },
+});
